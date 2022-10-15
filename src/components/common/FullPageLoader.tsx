ddd812124dac546a7824494babeba5edf41145fe
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2022-10-15 19:01:35 +0300

    Added fullpage loader icon

--- src/components/common/FullPageLoader.tsx
+++ src/components/common/FullPageLoader.tsx
@@ -6,10 +6,11 @@ export const FullPageLoader = () => {
     <Dialog
       open
       sx={{ backdropFilter: 'blur(4px)', backgroundColor: 'rgba(255, 255, 255, 0.3)' }}
-      PaperProps={{ sx: { background: 'transparent', boxShadow: 'none' } }}
+      PaperProps={{ sx: { background: 'transparent', boxShadow: 'none', overflowY: 'hidden' } }}
       hideBackdrop
     >
       <FullPageLoaderStyled>
+        <LoaderIcon src='/loader-icon.svg' alt='loader-icon' />
         <LoaderText>Almost there...</LoaderText>
       </FullPageLoaderStyled>
     </Dialog>
@@ -20,8 +21,20 @@ const FullPageLoaderStyled = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+`
+
+const LoaderIcon = styled.img`
+  display: block;
+  width: 75px;
+  height: 75px;
+  margin-bottom: 20px;
 `
 
 const LoaderText = styled.span`
   display: block;
+  font-family: ${({ theme }) => theme.fonts.futuraPT};
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 14px;
 `
